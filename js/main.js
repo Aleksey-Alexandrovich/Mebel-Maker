a5f7746ff386ba2e76dec54c6e70e362c27a8c0c
Aleksey <alexeyegorov1987@mail.ru>
2022-11-09 23:38:28 +0300

    add new files

--- js/main.js
+++ js/main.js
@@ -1,25 +1,10 @@
 $(document).ready(function() {
 	$('.slider').slick({
 		adaptiveheight: true,
-		slidesToShow:3,
-		responsive:[
-			{
-				breakpoint: 768,
-				settings: {
-					slidesToShow:2
-
-				},
-				breakpoint: 425,
-				settings: {
-					slidesToShow:1,
-					dots: true
-
-				}
-
-			}
-
-		]
-
+		slidesToShow: 3,
+		centerMode: true,
+		centerPadding: '20px'
+		
 	});
 
 });
